4d34ab9d36bc4a0e5dac6757c5e2f61acbae3328
Batbaina Guikoura <73786354+Batbaina@users.noreply.github.com>
2025-02-03 06:11:27 +0100

    Update model.py

--- model.py
+++ model.py
@@ -30,17 +30,18 @@ def plot_predictions(train_data, train_labels,  test_data, test_labels,  predict
 
 
 def mae(y_test, y_pred):
-  """
-  Calculuates mean absolute error between y_test and y_preds.
-  """
-  return tf.metrics.mean_absolute_error(y_test, y_pred)
-  
+    """
+    Calculates mean absolute error between y_test and y_preds.
+    """
+    return tf.keras.metrics.mean_absolute_error(y_test, y_pred)
+
 
 def mse(y_test, y_pred):
-  """
-  Calculates mean squared error between y_test and y_preds.
-  """
-  return tf.metrics.mean_squared_error(y_test, y_pred)
+    """
+    Calculates mean squared error between y_test and y_preds.
+    """
+    return tf.keras.metrics.mean_squared_error(y_test, y_pred)
+
 
 
 # Check Tensorflow version
